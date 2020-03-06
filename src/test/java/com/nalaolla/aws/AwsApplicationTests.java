378039856f1525b0695ce75521f58639d9334437
nalaolla <palaoh@gmail.com>
2020-03-06 18:50:10 +0900

    Initial commit

---
+++ src/test/java/com/nalaolla/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalaolla.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
