47a3037be07b0525a5a88982a3a55ae896811f0c
daniel,kim <60847008+nalaolla@users.noreply.github.com>
2020-03-06 19:10:52 +0900

    main

---
+++ src/main/java/com/nalaolla/aws/controller/DefaultController.java
@@ -0,0 +1,17 @@
+package com.nalaolla.aws.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class DefaultController {
+
+    @GetMapping("main")
+    public String main(Model model) {
+        return "main";
+
+    }
+}
